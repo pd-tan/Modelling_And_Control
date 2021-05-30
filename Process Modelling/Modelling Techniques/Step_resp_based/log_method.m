de0f5005598ceb638ef78299f2bae9f33cce63bf
Tan Pin Da <tan.pin.da@gmail.com>
2021-05-30 17:40:07 +0800

    update TPM and log to fit FOPTD class

--- Process Modelling/Modelling Techniques/Step_resp_based/log_method.m
+++ Process Modelling/Modelling Techniques/Step_resp_based/log_method.m
@@ -1,9 +1,13 @@
-function [params,log_graph] = log_method(recorded_data,steady_state_val)
+function [out_FOPTD,log_graph] = log_method(recorded_data,steady_state_val)
+
+%% Import Tools
+import_tools();
+
 
 time = recorded_data.time;
 resp = recorded_data.data;
 
-y_temp = (steady_state_val - resp)/steady_state_val  ;
+y_temp = (steady_state_val - resp)/steady_state_val;
 y = log(y_temp);
 
 max(time);
@@ -11,10 +15,11 @@ coefficients = polyfit(time, y, 1);
 xFit = linspace(min(time), max(time), 1000);
 yFit = polyval(coefficients , xFit);
 
+K = steady_state_val;
+tau = -1/coefficients(1);
+L = -coefficients(2)/coefficients(1);
+out_FOPTD = FOPTD_system(K,tau,L)
 
-params.tau = -1/coefficients(1);
-params.K = steady_state_val;
-params.L = -coefficients(2)/coefficients(1);
 log_graph.coeff = coefficients;
 log_graph.time = time;
 log_graph.data = y;
--- Process Modelling/Modelling Techniques/Step_resp_based/two_point_method.m
+++ Process Modelling/Modelling Techniques/Step_resp_based/two_point_method.m
@@ -1,11 +1,16 @@
-function params = two_point_method(recorded_data,steady_state_val,p_1,p_2)
+function out_FOPTD = two_point_method(recorded_data,steady_state_val,p_1,p_2)
 %TWO_POINT_METHOD Using two point on the step response curve, models the
 %system as a FOPTD system.
 %   Based on the selected percetages, the time which the values occurs
 %   should satisfy the equation: -ln(1-percentage)T +L. Based on this, two
 %   equtions are created, and a solution to the unknown L and T are found.
-    time = recorded_data.time;
-    resp = recorded_data.data;
+
+
+%% Import Tools
+import_tools()
+
+time = recorded_data.time;
+resp = recorded_data.data;
 chosen_percentages = [p_1,p_2];
 actual_percentages = [];
 time_samples = [];
@@ -21,6 +26,7 @@ for i = [1 2]
     coeff(i) = -1*log(1-actual_percentage(i));
 end
 
-params.K = steady_state_val;
-params.tau = (time_samples(1) - time_samples(2))/(coeff(1)-coeff(2));
-params.L = time_samples(1)-coeff(1)*params.tau;
+K = steady_state_val;
+tau = (time_samples(1) - time_samples(2))/(coeff(1)-coeff(2));
+L = time_samples(1)-coeff(1)*tau;
+out_FOPTD = FOPTD_system(K,tau,L)
